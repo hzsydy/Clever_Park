52c0aa8efa38b47c9820677dfd21b8b5a67b1c5f
Du <hzsydy@foxmail.com>
2016-08-22 10:47:20 +0800

    【ADD】draw L298N

--- vhdl_lib/UART/UART.vhd
+++ vhdl_lib/UART/UART.vhd
@@ -1,6 +1,8 @@
 library IEEE;
     use IEEE.std_logic_1164.all;
     use IEEE.numeric_std.all;
+    use IEEE.std_logic_unsigned.all;
+    use IEEE.std_logic_arith.all;
 
 entity UART is
     generic (
